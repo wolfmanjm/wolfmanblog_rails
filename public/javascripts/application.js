dfdc6ea60eaaa39df269b989926cbc7cd9effe27
Jim Morris <morris@wolfman.com>
2010-11-15 00:07:28 -0800

    switch over to jquery fixes comment url/email

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+	$("a#leave_email").click(function(event){
+		$("#guest_url").toggle();
+		$("#guest_email").toggle();
+		event.preventDefault();
+	});
+});
