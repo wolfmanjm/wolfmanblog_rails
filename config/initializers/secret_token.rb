b84c0db375419ac51115717990b8f3b5829bbb10
Jim Morris <morris@wolfman.com>
2010-11-16 18:29:18 -0800

    Upgrade to rails 3.0.3

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Wolfmanblog::Application.config.secret_token = 'e287751d62070b957f8b947d13823e202836319cb613a945a9400a8407b3956bbdb562e73a839e69387e402a778c4eb58687292b61250fef69d9501c93ffd210'
+Wolfmanblog::Application.config.secret_token = 'ba2931406936d4e67841ba5e1884f2d72aeed461d81d6c66d4b3aeb6a9d73a76604c9b10d82cc1e55fe7dc75a91e8eb248ff01adcfae42d4f6fcafa7315c0838'
