4c095a7e04a13e05c5496cf99941debe620b262b
Jim Morris <morris@wolfman.com>
2010-11-22 03:04:18 -0800

    use rotating log file for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Wolfmanblog::Application.configure do
   # config.log_level = :debug
 
   # Use a different logger for distributed setups
-  # config.logger = SyslogLogger.new
+  config.logger = Logger.new(config.paths.log.first, 10, 1048576)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
