bb180d41d201c6176098a8fe3473c48aa8d5e9f6
Jim Morris <morris@wolfman.com>
2010-12-28 15:25:52 -0800

    refactor spec

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -6,21 +6,17 @@ describe PostsHelper do
     @post= mock_model(Post)
   end
   
-  describe '#num_comments with comments' do
+  describe '#num_comments' do
     it "should have 10 comments" do
       @post.stub("comments_size").and_return(10)
       # <a href=\"/post/1001#comments\">10 comments</a>
       helper.num_comments(@post).should have_selector('a', :href => "/post/#{@post.id}#comments", :content => '10 comments')
     end
-  end
 
-  describe '#num_comments with no comments' do
     it 'should have no comments' do
       @post.stub("comments_size").and_return(0)
       helper.num_comments(@post).should == 'no comments'
     end
   end
-
-  
   
 end
