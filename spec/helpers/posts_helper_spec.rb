c8f224bc761575120fc3bb9bdf832f3549593d5e
Jim Morris <morris@wolfman.com>
2010-12-28 15:36:30 -0800

    added specs for rest of post helpers methods

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -18,5 +18,49 @@ describe PostsHelper do
       helper.num_comments(@post).should == 'no comments'
     end
   end
+
+  describe '#categories' do
+    before do
+      @post= mock_model(Post)
+    end
+    
+    it 'should have no categories' do
+      @post.stub(:categories).and_return([])
+      helper.categories(@post).should be_blank
+    end
+
+    it 'should have categories' do
+      c1= double
+      c2= double
+      c1.stub(:name).and_return('cat1')
+      c2.stub(:name).and_return('cat2')
+      @post.stub(:categories).and_return([c1, c2])
+      # <a href=\"/articles/category/cat1\">cat1</a>,<a href=\"/articles/category/cat2\">cat2</a>
+      helper.categories(@post).should have_selector('a', :href => "/articles/category/cat1", :content => 'cat1')
+      helper.categories(@post).should have_selector('a', :href => "/articles/category/cat2", :content => 'cat2')
+    end    
+  end
+
+  describe '#tags' do
+    before do
+      @post= mock_model(Post)
+    end
+    
+    it 'should have no tags' do
+      @post.stub(:tags).and_return([])
+      helper.tags(@post).should be_blank
+    end
+
+    it 'should have tags' do
+      c1= double
+      c2= double
+      c1.stub(:name).and_return('tag1')
+      c2.stub(:name).and_return('tag2')
+      @post.stub(:tags).and_return([c1, c2])
+      helper.tags(@post).should have_selector('a', :href => "/articles/tag/tag1", :content => 'tag1')
+      helper.tags(@post).should have_selector('a', :href => "/articles/tag/tag2", :content => 'tag2')
+    end    
+  end
   
+
 end
